24c86d370e97a8e5167d79d8a649c0691bf33927
HanxunHuangLemonBear <curtishuang@foxmail.com>
2020-02-10 12:03:30 +1100

    [Remove]Graph&Script

--- slurm/cifar10/archieve/asym/all.sh
+++
@@ -1,7 +0,0 @@
-declare -a loss_arr=("nfocal")
-
-for i in "${loss_arr[@]}"
-do
-  job_name=${i}_${1}
-  sbatch --partition deeplearn --cpus-per-task=2 --gres=gpu:v100:1 --qos gpgpudeeplearn --job-name $job_name --mem=16G ${i}.slurm $1 $2
-done
--- slurm/clothing1m/all.sh
+++
@@ -1,6 +0,0 @@
-# NLNL
-sbatch --partition deeplearn --gres=gpu:v100:1 --time 30:00:00 --qos gpgpudeeplearn ce.slurm
-sbatch --partition deeplearn --gres=gpu:v100:1 --time 30:00:00 --qos gpgpudeeplearn sce.slurm
-sbatch --partition deeplearn --gres=gpu:v100:1 --time 30:00:00 --qos gpgpudeeplearn gce.slurm
-sbatch --partition deeplearn --gres=gpu:v100:1 --time 30:00:00 --qos gpgpudeeplearn nce_rce.slurm
-sbatch --partition deeplearn --gres=gpu:v100:1 --time 30:00:00 --qos gpgpudeeplearn nfl_rce.slurm
