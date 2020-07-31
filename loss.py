0c207f77b8139a6af4c47a1a995e8cf30cca4e60
HanxunHuangLemonBear <curtishuang@foxmail.com>
2020-07-31 18:21:55 +1000

    [UPDATE] Note on MAE

--- loss.py
+++ loss.py
@@ -138,6 +138,12 @@ class MeanAbsoluteError(torch.nn.Module):
         pred = F.softmax(pred, dim=1)
         label_one_hot = torch.nn.functional.one_hot(labels, self.num_classes).float().to(self.device)
         mae = 1. - torch.sum(label_one_hot * pred, dim=1)
+        # Note: Reduced MAE
+        # Original: torch.abs(pred - label_one_hot).sum(dim=1)
+        # $MAE = \sum_{k=1}^{K} |\bm{p}(k|\bm{x}) - \bm{q}(k|\bm{x})|$
+        # $MAE = \sum_{k=1}^{K}\bm{p}(k|\bm{x}) - p(y|\bm{x}) + (1 - p(y|\bm{x}))$
+        # $MAE = 2 - 2p(y|\bm{x})$
+        #
         return self.scale * mae.mean()
 
 
